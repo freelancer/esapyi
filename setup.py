a77e33024e1bd102a59867c0d88bd5e6cdbedbee
Adishwar Rishi <arishi@freelancer.com>
2019-08-25 18:43:05 +1000

    Fix project name in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='api_boilerplate',
+    name='esapyi',
     version='0.1',
     description='A python API boilerplate',
     author='Adishwar Rishi',
