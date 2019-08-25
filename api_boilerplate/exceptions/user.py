99ddcb40d960a19425f6f038d6e6a6d0bb4facf3
Adishwar Rishi <arishi@freelancer.com>
2019-08-25 14:31:26 +1000

    Small code cleanup with exceptions

--- api_boilerplate/exceptions/user.py
+++ api_boilerplate/exceptions/user.py
@@ -1,15 +1,5 @@
-from typing import Optional
-
 class UserNotFoundException(Exception):
-    def __init__(self, message: Optional[str] = None) -> None:
-        if message:
-            super().__init__(message)
-        else:
-            super().__init__()
+    pass
 
 class UserAlreadyExistsException(Exception):
-    def __init__(self, message: Optional[str] = None) -> None:
-        if message:
-            super().__init__(message)
-        else:
-            super().__init__()
+    pass
