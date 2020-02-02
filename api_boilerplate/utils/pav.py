6c812a4fc98658e7a13b817ca229bc1a37a797d7
Adishwar Rishi <adiswa123@gmail.com>
2020-02-02 11:17:55 +1100

    Update all python packages and bump python version to 3.8

--- api_boilerplate/utils/pav.py
+++ api_boilerplate/utils/pav.py
@@ -1,13 +1,13 @@
-from typing import Any
+from typing import Any, cast
 from pavlova.flask import FlaskPavlova
 from pavlova.parsers import GenericParser
 
 
 class Email(str):
-    def __new__(cls, input_value: Any) -> str:
+    def __new__(cls, input_value: Any) -> 'Email':
         if isinstance(input_value, str):
             if '@' in input_value:
-                return str(input_value)
+                return cast(Email, str(input_value))
             raise ValueError()
         raise TypeError()
 
